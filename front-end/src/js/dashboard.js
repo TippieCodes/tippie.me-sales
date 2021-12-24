0489593f4c9338f16a367debb9132a32b9390ce0
Tippie <rowan@tippie.me>
2021-12-24 12:44:28 +0100

    Fixed nav urls

--- front-end/src/js/dashboard.js
+++ front-end/src/js/dashboard.js
@@ -48,7 +48,7 @@ function load() {
                 if (client.role["permission_list_employees"]) {
                     navBar[6]="<li class=\"nav-item\" id=\"nav-employees\">\n" +
                         "                        <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->\n" +
-                        "                        <a class=\"nav-link\" href=\"employees\">\n" +
+                        "                        <a class=\"nav-link\" href=\""+ root_url+"employees\">\n" +
                         "                            <span class=\"nav-icon\">\n" +
                         "                                <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-people\" viewBox=\"0 0 16 16\">\n" +
                         "                                  <path d=\"M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z\"/>\n" +
@@ -63,7 +63,7 @@ function load() {
                 if (client.role["permission_overview"]) {
                     navBar[1]=`<li class="nav-item">
                         <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
-                        <a class="nav-link" href="/">
+                        <a class="nav-link" href="${root_url}">
                             <span class="nav-icon">
                                 <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-house-door"
                                     fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -82,7 +82,7 @@ function load() {
                     navBar[2]=`<!--//nav-item-->
                     <li class="nav-item">
                         <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
-                        <a class="nav-link" href="sell">
+                        <a class="nav-link" href="${root_url}sell">
                             <span class="nav-icon">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cash-stack" viewBox="0 0 16 16">
                                     <path d="M1 3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1H1zm7 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/>
@@ -97,7 +97,7 @@ function load() {
                 if (client.role["permission_list_stock"]){
                     navBar[3]=`<li class="nav-item">
                         <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
-                        <a class="nav-link" href="stock">
+                        <a class="nav-link" href="${root_url}stock">
                             <span class="nav-icon">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                     class="bi bi-box-seam" viewBox="0 0 16 16">
@@ -113,7 +113,7 @@ function load() {
                 if (client.role["permission_list_shifts"]) {
                     navBar[4]=`<li class="nav-item">
                         <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
-                        <a class="nav-link" href="shifts">
+                        <a class="nav-link" href="${root_url}shifts">
                             <span class="nav-icon">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock-history" viewBox="0 0 16 16">
                                     <path d="M8.515 1.019A7 7 0 0 0 8 1V0a8 8 0 0 1 .589.022l-.074.997zm2.004.45a7.003 7.003 0 0 0-.985-.299l.219-.976c.383.086.76.2 1.126.342l-.36.933zm1.37.71a7.01 7.01 0 0 0-.439-.27l.493-.87a8.025 8.025 0 0 1 .979.654l-.615.789a6.996 6.996 0 0 0-.418-.302zm1.834 1.79a6.99 6.99 0 0 0-.653-.796l.724-.69c.27.285.52.59.747.91l-.818.576zm.744 1.352a7.08 7.08 0 0 0-.214-.468l.893-.45a7.976 7.976 0 0 1 .45 1.088l-.95.313a7.023 7.023 0 0 0-.179-.483zm.53 2.507a6.991 6.991 0 0 0-.1-1.025l.985-.17c.067.386.106.778.116 1.17l-1 .025zm-.131 1.538c.033-.17.06-.339.081-.51l.993.123a7.957 7.957 0 0 1-.23 1.155l-.964-.267c.046-.165.086-.332.12-.501zm-.952 2.379c.184-.29.346-.594.486-.908l.914.405c-.16.36-.345.706-.555 1.038l-.845-.535zm-.964 1.205c.122-.122.239-.248.35-.378l.758.653a8.073 8.073 0 0 1-.401.432l-.707-.707z"/>
@@ -232,9 +232,9 @@ function checkPage() {
                     </a><!--//nav-link-->
                     <div id="submenu-casino" class="collapse submenu submenu-1" data-parent="#nav-bar">
                         <ul class="submenu-list list-unstyled">
-                            ${(client.role["permission_casino_cards"] == true) ? `<li class="submenu-item"><a class="submenu-link" href="casino/cards">Card Management</a></li>` : ""}
-                            ${(client.role["permission_casino_bingo"] == true) ? `<li class="submenu-item"><a class="submenu-link" href="casino/bingo">Bingo</a></li>` : ""}
-                            ${(client.role["permission_casino_blackjack"] == true) ? `<li class="submenu-item"><a class="submenu-link" href="casino/blackjack">Blackjack</a></li>` : ""}
+                            ${(client.role["permission_casino_cards"] == true) ? `<li class="submenu-item"><a class="submenu-link" href="${root_url}casino/cards">Card Management</a></li>` : ""}
+                            ${(client.role["permission_casino_bingo"] == true) ? `<li class="submenu-item"><a class="submenu-link" href="${root_url}casino/bingo">Bingo</a></li>` : ""}
+                            ${(client.role["permission_casino_blackjack"] == true) ? `<li class="submenu-item"><a class="submenu-link" href="${root_url}casino/blackjack">Blackjack</a></li>` : ""}
                                                 <!--<li class="submenu-item"><a class="submenu-link" href="casino/war">War</a></li>-->
                         </ul>
                     </div>
